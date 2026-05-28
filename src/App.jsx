aec895fadd78fb0cdc6f941dd8e349a8d7f18749
Mohan-Nimmakayala <mohannims223@gmail.com>
2026-05-28 12:12:23 +0530

    router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Section from './components/Section'
-import Doctorcard from './components/Doctorcard'
-import AddDoctor from './components/AddDoctor'
-import { useState, useEffect } from 'react'
+import Navbar from "./components/Navbar"
+import Section from "./components/Section"
 
+import Doctorcard from "./components/Doctorcard"
+import Adddoctor from "./components/Adddoctor"
+import { Route, Router, Routes } from "react-router-dom"
+// import { useState } from "react"
+// import { useEffect } from "react"
 function App() {
-  let[count, setCount] = useState(0)
-  function addcount() {
-    setCount(count+1)
-    console.log(count)
-  }
-  useEffect(()=>{
-    console.log("useEffect is running")
-  },[count])
+
+  // let [count,setCount]=useState(0)
+  // function addcount(){
+  //   setCount(count+1)
+  //   console.log(count)
+  // }
+  // useEffect(()=>{
+  //   console.log('useeffect is running')
+  // },[count])
+
   return (
     <div>
-      {count}
-      <button onClick={addcount}>Click</button>
+       {/* {count}
+      <button  onClick={addcount}>click to increase</button>  */}
       <Navbar/>
-      <Section/>
+      <Routes>
+        <Route path='/' element={<Section/>}/>
+        <Route path='/add-doctor' element={<Adddoctor/>}/>
+      </Routes>
+      {/* <Section/> */}
       <div className='doctorcontainer'>
-        <Doctorcard name='Ramakrishna' specialization='Rheumatology' gender='Male'/>
-        <Doctorcard name='Prasad' specialization='General' gender='Male'/>
-        <Doctorcard name='Shivani' specialization='Cosmetology' gender='Female'/>
-        <Doctorcard name='Krishna' specialization='Orthopedic' gender='Male'/>
+      <Doctorcard name='Vasavi' specialization='psychology' gender='female'/>
+      <Doctorcard name='Vamsi' specialization='heart' gender='male'/>
+      <Doctorcard name='Deja' specialization='muscles' gender='male'/>
+      <Doctorcard name='Surya' specialization='ENT' gender='male'/>
+      <Doctorcard name='Deekshit' specialization='cardio' gender='male'/>
+      
       </div>
-      <AddDoctor/>
+     {/* <Adddoctor/> */}
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import './styles.css'
 
 function Navbar() {
+  let navigate=useNavigate()
   return (
     <nav>
         <h1>Doctor App</h1>
-        <h3>Home | Add Doctor | Contact</h3>
+        <h3>
+          <div>
+            <button onClick={()=>navigate('/')}>Home</button>
+          </div>
+          <div>
+            <button onClick={()=>navigate('/add-doctor')}>Add doctor</button>
+          </div>
+        </h3>
     </nav>
   )
 }
